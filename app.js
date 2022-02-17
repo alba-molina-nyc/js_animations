53ff4a8544493b1eb9afffaecb97aa171830f229
Username <albavmolinanyc@gmail.com>
2022-02-17 18:19:50 -0500

    javascript is set up and working

---
+++ app.js
@@ -0,0 +1,4 @@
+// takes in 2 params, want to animate to a certain place
+// inside curly select y to select down, move down by 20 pixels, then define duration
+gsap.to('.text', {y: 20, duration: 1})
+console.log('hello bitch')
