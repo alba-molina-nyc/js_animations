197a2f385afe79ae43992d4be4fb527ed416a408
Username <albavmolinanyc@gmail.com>
2022-02-18 15:06:15 -0500

    cookies

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 // takes in 2 params, want to animate to a certain place
 // inside curly select y to select down, move down by 20 pixels, then define duration
-gsap.to('.text', {y: 20, duration: 1})
-console.log('hello bitch')
+// gsap.to('.text', {y: 20, opacity: 0, duration: 1})
+// console.log('hello bitch')
+
+// fromto takes in three properties
+gsap.fromTo('.text', {opacity: 0, y: 100}, {opacity: 1, y: 0, duration: 1})
