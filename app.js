f18a613ce0bf597b57b9c37ddcce4cba840b9757
Username <albavmolinanyc@gmail.com>
2022-02-19 16:21:00 -0500

    bounce

--- app.js
+++ app.js
@@ -7,6 +7,6 @@
 // inside timeline define how the animations will behave duration, delays etc.
 // const tl = gsap.timeline({defaults: {duration: 0.75}})
 
-gsap.fromTo('.cookie-container', {scale: 0}, {scale: 1})
+gsap.fromTo('.cookie-container', {scale: 0}, {scale: 1, ease: "elastic.out(1, 0.4)", duration: 1.5})
 gsap.fromTo('.cookie', {opacity: 0, x:-50, rotation: '-45deg'}, {opacity: 1, x:0, rotation: '0deg'})
 gsap.fromTo('.text', {x:30, opacity: 0}, {x:0, opacity: 1}, )
